9e3163007167ab8cc9e907061b17563714aab48b
idan haviv <idan.haviv@gmail.com>
2015-06-20 16:33:54 +0300

    removed some FB imports from bridging header for project to compile

--- Trainem-Bridging-Header.h
+++ Trainem-Bridging-Header.h
@@ -2,6 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <Parse/Parse.h>
-#import <ParseFacebookUtils/PFFacebookUtils.h>
+//#import <ParseFacebookUtils/PFFacebookUtils.h>
